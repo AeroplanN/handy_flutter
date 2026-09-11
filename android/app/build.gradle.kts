f2858e6df6ff3de5ccf9e2f38690516637c22fc7
AeroplanN <adlantagiyev@gmail.com>
2026-09-12 03:56:05 +0500

    37

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "computer.handy.handy"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
